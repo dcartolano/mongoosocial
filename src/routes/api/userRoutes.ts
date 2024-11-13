7bda4b23afd93e2d046e2dedce734d8e30788e27
dcartolano <180475468+dcartolano@users.noreply.github.com>
2024-11-13 16:12:42 -0600

    combined friend routes into the same line

--- src/routes/api/userRoutes.ts
+++ src/routes/api/userRoutes.ts
@@ -17,9 +17,6 @@ router.route('/').get(getAllUsers).post(createUser);
 router.route('/:userId').get(getUserById).put(updateUser).delete(deleteUser);
 
 // /api/users/:userId/friends/:friendId
-router.route('/:userId/friends/:friendId').post(addFriend);
-
-// /api/users/:userId/friends/:friendId
-router.route('/:userId/friends/:friendId').delete(removeFriend);
+router.route('/:userId/friends/:friendId').post(addFriend).put(removeFriend);
 
 export { router as userRouter} ;
