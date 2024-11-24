eb3ac3428b0e987484a4f084d7524d6d3c946e76
dcartolano <180475468+dcartolano@users.noreply.github.com>
2024-11-24 16:41:59 -0600

    changed port back to 3002

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import db from './config/connection.js';
 
 await db();
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 const app = express();
 
 app.use(express.urlencoded({ extended: true}));
